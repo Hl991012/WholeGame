e99992a6f8f407009139e1ff81ea153b8a41849b
lienlai <1057344680@qq.com>
2024-10-10 19:57:04 +0800

    dataSave

---
+++ Assets/Game/Common/Scripts/DataSave/GameDataSaveBaseModel.cs
@@ -0,0 +1,6 @@
+public class GameDataSaveBaseModel
+{
+    public int BestScore { get; set; }
+
+    public int StageLevel { get; set; }
+}
---
+++ Assets/Game/Common/Scripts/DataSave/GameDataSaveHelper.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using GameFrame;
+
+public class GameDataSaveHelper : Singleton<GameDataSaveHelper>
+{
+    private Dictionary<GameType, object> allGameData;
+    
+    // public T GetData<>
+}
